12a1a8001ca68573c62d30e4542b481e1c9fc4f1
ronks50 <ronks@mail.ru>
2019-01-08 12:41:05 +0300

    Decommonize sdm845 tree. We are specific to beryllium now.

---
+++ superior_beryllium.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/beryllium/device.mk)
+
+# Inherit some common Superior stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := superior_beryllium
+PRODUCT_DEVICE := beryllium
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Pocophone F1
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/V10.1.3.0.PEJMIFI:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="beryllium-user 9 PKQ1.180729.001 V10.1.3.0.PEJMIFI release-keys" \
+    PRODUCT_NAME="beryllium" \
+    TARGET_DEVICE="beryllium"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
